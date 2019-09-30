2fb957bd96c7098795b9fb367e42bba338f0af00
Roland Guijt <roland.guijt@gmail.com>
2019-09-30 08:54:18 +0200

    Added mitigations

--- WiredBrain.CustomerPortal.AspNet/HeaderHelpers/IAppBuilderExtensions.cs
+++ WiredBrain.CustomerPortal.AspNet/HeaderHelpers/IAppBuilderExtensions.cs
@@ -8,7 +8,16 @@ namespace WiredBrain.CustomerPortal.AspNet.HeaderHelpers
         {
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("HeaderType", new[] { "HeaderValue" });
+                context.Response.Headers.Add(
+                    "Content-Security-Policy", new[] {"style-src 'self' " +
+                    "https://stackpath.bootstrapcdn.com;" +
+                    "frame-ancestors 'none'" });
+                context.Response.Headers.Add(
+                    "Feature-Policy", new[] { "camera 'none'" });
+                context.Response.Headers.Add(
+                    "X-Content-Type-Options", new[] { "nosniff" });
+                context.Response.Headers.Add(
+                    "Referrer-Policy", new[] { "no-referrer" });
                 await next();
             });
         }
